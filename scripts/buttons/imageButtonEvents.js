d1e1d8f4e79e6478c9cc061d31274db45bbf499d
thegreatgramblinham <samhudock@gmail.com>
2016-05-23 21:00:00 -0400

    Adding button highlighting for mode.

--- scripts/buttons/imageButtonEvents.js
+++ scripts/buttons/imageButtonEvents.js
@@ -7,3 +7,19 @@ function ImageButtonOnClick(imageIndex)
 }
 
 //todo set selection color
+
+addModeButton.onclick = function(e)
+{
+    CanvasMode = UIMode.Add;
+    console.log("Switched to image ADD mode.");
+    document.getElementById("addModeButton").style.backgroundColor = "red";
+    document.getElementById("modfiyModeButton").style.backgroundColor = "darkgrey";
+}
+
+modfiyModeButton.onclick = function(e)
+{
+    CanvasMode = UIMode.Modify;
+    console.log("Switched to image MODIFY mode.");
+        document.getElementById("addModeButton").style.backgroundColor = "darkgrey";
+    document.getElementById("modfiyModeButton").style.backgroundColor = "red";
+}
