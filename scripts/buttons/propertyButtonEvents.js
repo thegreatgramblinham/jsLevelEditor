df5365667950250dddd12a3162e426832f8c4589
thegreatgramblinham <samhudock@gmail.com>
2016-05-25 20:06:28 -0400

    Added width and height events to affect the selected rect

--- scripts/buttons/propertyButtonEvents.js
+++ scripts/buttons/propertyButtonEvents.js
@@ -3,14 +3,18 @@
 //Private Variables
 var _xBox = document.getElementById("rectXBox");
 var _yBox = document.getElementById("rectYBox");
+var _widthBox = document.getElementById("rectWidthBox");
+var _heightBox = document.getElementById("rectHeightBox");
 
 function OnSelectedRectangleChanged()
 {
     _xBox.value = SelectedRectangle.XLocation;
     _yBox.value = SelectedRectangle.YLocation;
+    _widthBox.value = SelectedRectangle.Width;
+    _heightBox.value = SelectedRectangle.Height;
 }
 
-_xBox.oninput = function(e)
+_xBox.onchange = function(e)
 {
     if(SelectedRectangle != undefined)
     {
@@ -18,10 +22,26 @@ _xBox.oninput = function(e)
     }
 }
 
-_yBox.oninput = function(e)
+_yBox.onchange = function(e)
 {
     if(SelectedRectangle != undefined)
     {
         SelectedRectangle.YLocation = _yBox.value;
+    }
+}
+
+_widthBox.onchange = function(e)
+{
+    if(SelectedRectangle != undefined)
+    {
+        SelectedRectangle.Width = _widthBox.value;
+    }
+}
+
+_heightBox.onchange = function(e)
+{
+    if(SelectedRectangle != undefined)
+    {
+        SelectedRectangle.Height = _heightBox.value;
     }
 }
