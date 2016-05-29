d9cdf9af9dc5979637922780c8fcc39f659c7a2f
thegreatgramblinham <samhudock@gmail.com>
2016-05-29 15:51:39 -0400

    Switching over to modular property restore method.

--- scripts/importing/pldImporter.js
+++ scripts/importing/pldImporter.js
@@ -6,6 +6,7 @@ var FILE_EXTENSION = "pld";
 var LEVEL_TAG = "Stage";
 var GLOBAL_TAG = "Global";
 var LEVEL_SIZE_TAG = "LevelSize";
+var OBJECT_TAG = "Objects";
 var BACKDROP_TAG = "Backdrop";
 var FLOOR_TAG = "Floor";
 var WALL_TAG = "Wall";
@@ -19,6 +20,7 @@ var X_TAG = "X";
 var Y_TAG = "Y";
 var WIDTH_TAG = "Width";
 var HEIGHT_TAG = "Height";
+var RENDER_TAG = "RenderLayer";
 
 var TYPE_TAG = "Type";
 var IMAGERECT_CLASS = "ImageRectangle";
@@ -95,47 +97,68 @@ class PLDImporter
         
         if(backgroundContainer == undefined) return; //no background present
         
-        var fileName = backgroundContainer.children[0].tagName;  
         var background = backgroundContainer.children[0];
+         
+        var propertyArr = this.DeserializeRectProperties(background);
         
-        var type;
-        var x;
-        var y;       
-        for(var i = 0; i < background.children.length; i++)
+        if(propertyArr[TYPE_TAG] == IMAGERECT_CLASS)
         {
-            var propertyTag = background.children[i];
-            
-            if(propertyTag.tagName == TYPE_TAG)
-                type = propertyTag.innerHTML;
-            
-            if(propertyTag.tagName == X_TAG)
-                x = Number(propertyTag.innerHTML);
-                
-            if(propertyTag.tagName == Y_TAG)
-                y = Number(propertyTag.innerHTML);
-        }
-        
-        if(type == undefined || type == "")
-            throw "Type not found.";
-        if(x == undefined || isNaN(x))
-            throw "X not found.";
-        if(y == undefined || isNaN(y))
-            throw "Y not found.";
-        
-        if(type == IMAGERECT_CLASS)
-        {
-            var image = GetImageElementByFileName(fileName);    
+            var image = GetImageElementByFileName(background.tagName);    
             if(image == undefined)
-                throw "Could not find background "+fileName+" in open images.";
+                throw "Could not find background "+ background.tagName +" in open images.";
                
-            AddImage(x, y, image, fileName);
+            AddImage(propertyArr[X_TAG], propertyArr[Y_TAG], image, background.tagName);
         }
-        else if(type == BASICRECT_CLASS)
+        else if(propertyArr[TYPE_TAG] == BASICRECT_CLASS)
         {
             
         }
         else   
             throw "Rectangle type not intializable."
 
+    }
+    
+    //Private Methods
+    DeserializeRectProperties(rectTag)
+    {    
+        var propertyArr = [];
+        
+        for(var i = 0; i < rectTag.children.length; i++)
+        {
+            var propertyTag = rectTag.children[i];
+            
+            if(propertyTag.tagName == TYPE_TAG)
+                propertyArr[TYPE_TAG] = propertyTag.innerHTML;
+            
+            if(propertyTag.tagName == X_TAG)
+                propertyArr[X_TAG] = Number(propertyTag.innerHTML);
+                
+            if(propertyTag.tagName == Y_TAG)
+                propertyArr[Y_TAG] = Number(propertyTag.innerHTML);
+                
+            if(propertyTag.tagName == WIDTH_TAG)
+                propertyArr[WIDTH_TAG] = Number(propertyTag.innerHTML);
+                
+            if(propertyTag.tagName == HEIGHT_TAG)
+                propertyArr[HEIGHT_TAG] = Number(propertyTag.innerHTML);
+                
+            if(propertyTag.tagName == RENDER_TAG)
+                propertyArr[RENDER_TAG] = Number(propertyTag.innerHTML);
+        }
+        
+        if(propertyArr[TYPE_TAG] == undefined || propertyArr[TYPE_TAG] == "")
+            throw rectTag.tagName + " Type not found.";
+        if(propertyArr[X_TAG] == undefined || isNaN(propertyArr[X_TAG]))
+            throw rectTag.tagName + " X not found.";
+        if(propertyArr[Y_TAG] == undefined || isNaN(propertyArr[Y_TAG]))
+            throw rectTag.tagName + " Y not found.";
+        if(propertyArr[WIDTH_TAG] == undefined || isNaN(propertyArr[WIDTH_TAG]))
+            throw rectTag.tagName + " Width not found.";
+        if(propertyArr[HEIGHT_TAG] == undefined || isNaN(propertyArr[HEIGHT_TAG]))
+            throw rectTag.tagName + " Height not found.";
+        if(propertyArr[RENDER_TAG] == undefined || isNaN(propertyArr[RENDER_TAG]))
+            throw rectTag.tagName + " RenderGroup not found.";
+            
+        return propertyArr;
     }
 }
