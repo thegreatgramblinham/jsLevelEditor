428e2f5e0a8fa8ec7af43a16bca8e7f97b7d879e
thegreatgramblinham <samhudock@gmail.com>
2016-05-29 16:52:28 -0400

    Changed all my constants to be more unique for no possibility of conflict.

--- scripts/importing/pldImporter.js
+++ scripts/importing/pldImporter.js
@@ -1,30 +1,30 @@
 //This file is for the importing of pld files.
 
 //Private Constants
-var FILE_EXTENSION = "pld";
+var PLD_FILE_EXTENSION = "pld";
 
-var LEVEL_TAG = "Stage";
-var GLOBAL_TAG = "Global";
-var LEVEL_SIZE_TAG = "LevelSize";
-var OBJECT_TAG = "Objects";
-var BACKDROP_TAG = "Backdrop";
-var FLOOR_TAG = "Floor";
-var WALL_TAG = "Wall";
-var PROP_TAG = "Props";
-var ENEMY_TAG = "Enemies";
-var EXIT_TAG = "Exits";
-var ENTRY_POINT_TAG = "Entries";
+var PLD_LEVEL_TAG = "Stage";
+var PLD_GLOBAL_TAG = "Global";
+var PLD_LEVEL_SIZE_TAG = "LevelSize";
+var PLD_OBJECT_TAG = "Objects";
+var PLD_BACKDROP_TAG = "Backdrop";
+var PLD_FLOOR_TAG = "Floor";
+var PLD_WALL_TAG = "Wall";
+var PLD_PROP_TAG = "Props";
+var PLD_ENEMY_TAG = "Enemies";
+var PLD_EXIT_TAG = "Exits";
+var PLD_ENTRY_POINT_TAG = "Entries";
 //todo scripted event triggers.
 
-var X_TAG = "X";
-var Y_TAG = "Y";
-var WIDTH_TAG = "Width";
-var HEIGHT_TAG = "Height";
-var RENDER_TAG = "RenderLayer";
+var PLD_X_TAG = "X";
+var PLD_Y_TAG = "Y";
+var PLD_WIDTH_TAG = "Width";
+var PLD_HEIGHT_TAG = "Height";
+var PLD_RENDER_TAG = "RenderLayer";
 
-var TYPE_TAG = "Type";
-var IMAGERECT_CLASS = "ImageRectangle";
-var BASICRECT_CLASS = "NamedRectangle";
+var PLD_TYPE_TAG = "Type";
+var PLD_IMAGERECT_CLASS = "ImageRectangle";
+var PLD_BASICRECT_CLASS = "NamedRectangle";
 
 class PLDImporter
 {
@@ -63,7 +63,7 @@ class PLDImporter
     
     SetLevelBounds()
     {
-        var levelSize = this.xmlDoc.getElementsByTagName(LEVEL_SIZE_TAG);
+        var levelSize = this.xmlDoc.getElementsByTagName(PLD_LEVEL_SIZE_TAG);
         
         if(levelSize == undefined || levelSize.length != 1)
             throw "Level size doesn't exist or is invalid.";
@@ -79,10 +79,10 @@ class PLDImporter
         {
             var child = levelsizeTag.children[i];
             
-            if(child.tagName == WIDTH_TAG)
+            if(child.tagName == PLD_WIDTH_TAG)
                 width = Number(child.innerHTML);
                 
-            if(child.tagName == HEIGHT_TAG)
+            if(child.tagName == PLD_HEIGHT_TAG)
                 height = Number(child.innerHTML);
         }    
         
@@ -96,7 +96,7 @@ class PLDImporter
     
     ReconstituteBackground()
     {
-        var backgroundTags = this.xmlDoc.getElementsByTagName(BACKDROP_TAG);
+        var backgroundTags = this.xmlDoc.getElementsByTagName(PLD_BACKDROP_TAG);
         var backgroundContainer = backgroundTags[0];
         
         if(backgroundContainer == undefined) return; //no background present
@@ -104,12 +104,12 @@ class PLDImporter
         var background = backgroundContainer.children[0];      
         var propertyArr = this.DeserializeRectProperties(background);
         
-        this.AddToCanvas(propertyArr, background, BACKDROP_TAG);
+        this.AddToCanvas(propertyArr, background, PLD_BACKDROP_TAG);
     }
     
     ReconstituteFloor()
     {
-        var floorTags = this.xmlDoc.getElementsByTagName(FLOOR_TAG);
+        var floorTags = this.xmlDoc.getElementsByTagName(PLD_FLOOR_TAG);
         var floorContainer = floorTags[0];
         
         if(floorContainer == undefined) return; //no floor present
@@ -117,12 +117,12 @@ class PLDImporter
         var floor = floorContainer.children[0];      
         var propertyArr = this.DeserializeRectProperties(floor);
         
-        this.AddToCanvas(propertyArr, floor, FLOOR_TAG);
+        this.AddToCanvas(propertyArr, floor, PLD_FLOOR_TAG);
     }
     
     ReconstituteWall()
     {
-        var wallTags = this.xmlDoc.getElementsByTagName(WALL_TAG);
+        var wallTags = this.xmlDoc.getElementsByTagName(PLD_WALL_TAG);
         var wallContainer = wallTags[0];
         
         if(wallContainer == undefined) return; //no wall present
@@ -130,12 +130,12 @@ class PLDImporter
         var wall = wallContainer.children[0];      
         var propertyArr = this.DeserializeRectProperties(wall);
         
-        this.AddToCanvas(propertyArr, wall, WALL_TAG);
+        this.AddToCanvas(propertyArr, wall, PLD_WALL_TAG);
     }
     
     ReconstituteProps()  
     {
-        var propTags = this.xmlDoc.getElementsByTagName(PROP_TAG);
+        var propTags = this.xmlDoc.getElementsByTagName(PLD_PROP_TAG);
         var propContainer = propTags[0];
         
         if(propContainer == undefined) return; //no props present
@@ -145,13 +145,13 @@ class PLDImporter
             var prop = propContainer.children[i];
 
             var propertyArr = this.DeserializeRectProperties(prop);       
-            this.AddToCanvas(propertyArr, prop, PROP_TAG);
+            this.AddToCanvas(propertyArr, prop, PLD_PROP_TAG);
         }
     }
     
     ReconstituteEnemies()
     {
-        var enemyTags = this.xmlDoc.getElementsByTagName(ENEMY_TAG);
+        var enemyTags = this.xmlDoc.getElementsByTagName(PLD_ENEMY_TAG);
         var enemyContainer = enemyTags[0];
         
         if(enemyContainer == undefined) return; //no enemy present
@@ -161,7 +161,7 @@ class PLDImporter
             var enemy = enemyContainer.children[i];
 
             var propertyArr = this.DeserializeRectProperties(enemy);       
-            this.AddToCanvas(propertyArr, enemy, ENEMY_TAG);
+            this.AddToCanvas(propertyArr, enemy, PLD_ENEMY_TAG);
         }
     }
     
@@ -174,36 +174,36 @@ class PLDImporter
         {
             var propertyTag = rectTag.children[i];
             
-            if(propertyTag.tagName == TYPE_TAG)
-                propertyArr[TYPE_TAG] = propertyTag.innerHTML;
+            if(propertyTag.tagName == PLD_TYPE_TAG)
+                propertyArr[PLD_TYPE_TAG] = propertyTag.innerHTML;
             
-            if(propertyTag.tagName == X_TAG)
-                propertyArr[X_TAG] = Number(propertyTag.innerHTML);
+            if(propertyTag.tagName == PLD_X_TAG)
+                propertyArr[PLD_X_TAG] = Number(propertyTag.innerHTML);
                 
-            if(propertyTag.tagName == Y_TAG)
-                propertyArr[Y_TAG] = Number(propertyTag.innerHTML);
+            if(propertyTag.tagName == PLD_Y_TAG)
+                propertyArr[PLD_Y_TAG] = Number(propertyTag.innerHTML);
                 
-            if(propertyTag.tagName == WIDTH_TAG)
-                propertyArr[WIDTH_TAG] = Number(propertyTag.innerHTML);
+            if(propertyTag.tagName == PLD_WIDTH_TAG)
+                propertyArr[PLD_WIDTH_TAG] = Number(propertyTag.innerHTML);
                 
-            if(propertyTag.tagName == HEIGHT_TAG)
-                propertyArr[HEIGHT_TAG] = Number(propertyTag.innerHTML);
+            if(propertyTag.tagName == PLD_HEIGHT_TAG)
+                propertyArr[PLD_HEIGHT_TAG] = Number(propertyTag.innerHTML);
                 
-            if(propertyTag.tagName == RENDER_TAG)
-                propertyArr[RENDER_TAG] = Number(propertyTag.innerHTML);
+            if(propertyTag.tagName == PLD_RENDER_TAG)
+                propertyArr[PLD_RENDER_TAG] = Number(propertyTag.innerHTML);
         }
         
-        if(propertyArr[TYPE_TAG] == undefined || propertyArr[TYPE_TAG] == "")
+        if(propertyArr[PLD_TYPE_TAG] == undefined || propertyArr[PLD_TYPE_TAG] == "")
             throw rectTag.tagName + " Type not found.";
-        if(propertyArr[X_TAG] == undefined || isNaN(propertyArr[X_TAG]))
+        if(propertyArr[PLD_X_TAG] == undefined || isNaN(propertyArr[PLD_X_TAG]))
             throw rectTag.tagName + " X not found.";
-        if(propertyArr[Y_TAG] == undefined || isNaN(propertyArr[Y_TAG]))
+        if(propertyArr[PLD_Y_TAG] == undefined || isNaN(propertyArr[PLD_Y_TAG]))
             throw rectTag.tagName + " Y not found.";
-        if(propertyArr[WIDTH_TAG] == undefined || isNaN(propertyArr[WIDTH_TAG]))
+        if(propertyArr[PLD_WIDTH_TAG] == undefined || isNaN(propertyArr[PLD_WIDTH_TAG]))
             throw rectTag.tagName + " Width not found.";
-        if(propertyArr[HEIGHT_TAG] == undefined || isNaN(propertyArr[HEIGHT_TAG]))
+        if(propertyArr[PLD_HEIGHT_TAG] == undefined || isNaN(propertyArr[PLD_HEIGHT_TAG]))
             throw rectTag.tagName + " Height not found.";
-        if(propertyArr[RENDER_TAG] == undefined || isNaN(propertyArr[RENDER_TAG]))
+        if(propertyArr[PLD_RENDER_TAG] == undefined || isNaN(propertyArr[PLD_RENDER_TAG]))
             throw rectTag.tagName + " RenderGroup not found.";
             
         return propertyArr;
@@ -211,18 +211,18 @@ class PLDImporter
     
     AddToCanvas(propertyArr, tag, category)
     {
-        if(propertyArr[TYPE_TAG] == IMAGERECT_CLASS)
+        if(propertyArr[PLD_TYPE_TAG] == PLD_IMAGERECT_CLASS)
         {
             var image = GetImageElementByFileName(tag.tagName);    
             if(image == undefined)
                 throw "Could not find file "+ tag.tagName +" in open images.";
                
-            AddImage(propertyArr[X_TAG], propertyArr[Y_TAG], image, tag.tagName, category);
+            AddImage(propertyArr[PLD_X_TAG], propertyArr[PLD_Y_TAG], image, tag.tagName, category);
         }
-        else if(propertyArr[TYPE_TAG] == BASICRECT_CLASS)
+        else if(propertyArr[PLD_TYPE_TAG] == PLD_BASICRECT_CLASS)
         {
-            AddRectangle(propertyArr[X_TAG], propertyArr[Y_TAG], propertyArr[WIDTH_TAG],
-                propertyArr[HEIGHT_TAG], tag.tagName, category);
+            AddRectangle(propertyArr[PLD_X_TAG], propertyArr[PLD_Y_TAG], propertyArr[PLD_WIDTH_TAG],
+                propertyArr[PLD_HEIGHT_TAG], tag.tagName, category);
                 
             //todo support for render groups
         }
